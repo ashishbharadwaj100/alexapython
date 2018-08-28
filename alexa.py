94c58224c3d2424f9b9daaac797b4b936244a016
Ashish Bharadwaj <ashishbharadwaj100@gmail.com>
2018-08-28 10:32:05 +0530

    commit after port

--- alexa.py
+++ alexa.py
@@ -68,15 +68,13 @@ def setTemp(TempValue):
     return statement('Ok. Control My Spa Set temperature to {} degree fahrenheit'.format(TempValue))
 
 
-
+'''
 if __name__ == '__main__':
     app.run(debug=True)
-
 '''
-# For Flask-ask in hero ku
+# For Flask-ask in heroku
 if __name__ == '__main__':
     #app.run()
     port = int(os.getenv('PORT', 5000))
     print ("Starting app on port %d" % port)
     app.run(debug=False, port=port, host='0.0.0.0')
-'''
